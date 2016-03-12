de45ff398ff2a48a7de173a19631cff395a292f3
Ex-Zy <serega1905@ukr.net>
2016-03-12 11:44:38 +0200

    upd

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,49 @@
 $(document).ready(function() {
+	// $('.slider').slick({
+	// 	slidesToShow: 1,
+	// 	cssEase: 'linear',
+	// 	fade: true,
+	// 	autoplay: true,
+	// 	autoplaySpeed: 10000,
+	// });
+	$('.js-time a').on('click', function(e) {
+		e.preventDefault();
+		var section = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(section).offset().top - 30
+		}, 500);
+		return false;
+	});
+	// social fixed
+	function stickySocial() {
+		if ($('.js-sticky').length) { 
+			var el = $('.js-sticky');
+			var stickyTop = $('.js-sticky').offset().top; 
+			var footerTop = $('.bottom').offset().top; 
+			var stickyHeight = $('.js-sticky').outerHeight();
+			// var limit = footerTop - stickyHeight;
+			var limit = footerTop + 215;
+			$(window).scroll(function(){ 
 
-	// go js
+				var windowTop = $(window).scrollTop();
+				if ($(window).width() >= 1024) {
+				
+					if (stickyTop < windowTop){
+						el.css({ position: 'fixed', top: 10 });
+						el.addClass('is-active');
+					}
+					else {
+						el.css('position','static');
+						el.removeClass('is-active');
+					}
 
+					if (limit < windowTop) {
+						var diff = limit - windowTop;
+						el.css({top: diff});
+					} 
+				} 
+			});
+		}
+	};
+	stickySocial();
 });
